bf99b5d8384db3b4a27e51df249d748c5476b9af
mehmetdemircioglu <112698694+mehmetdemircioglu@users.noreply.github.com>
2022-11-12 11:25:02 +0300

    jdbc connection

---
+++ src/Test11.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Test11 {
+
+    public static void main(String[] args) {
+
+        try{
+            String url = "jdbc:postgresql://localhost:5432/sbgm";
+            String username="postgres";
+            String password="root";
+
+            String sql_select="SELECT * FROM employee";
+
+            Connection conn = DriverManager.getConnection(url,username,password);
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sql_select);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+
+                System.out.println("Id :"+id+" Name :"+name+" Surname :"+surname);
+
+            }
+
+
+        }
+        catch (Exception ex) {
+
+        }
+
+
+    }
+}
